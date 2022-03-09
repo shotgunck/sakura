f3ab139502522b64428a9abe5b975edf9332f40f
shotgun <shotgunck137@gmail.com>
2022-03-09 20:15:52 +0700

    proper arguments passing

--- src/command_parser.rs
+++ src/command_parser.rs
@@ -7,7 +7,7 @@ use super::{minecraft, music, utilities};
 pub async fn parse(msg: &Message, ctx: &Context, thread: String) -> Result<(), Box<dyn Error + Send + Sync>> {
     let stuff: Vec<&str> = Regex::new(r" ").unwrap().split(&thread).collect();
     let cmd = stuff[0];
-    let args = if stuff.len() == 1 { "".into() } else { stuff[1].into() };
+    let args = Regex::new(&format!(r"{} ", &cmd)).unwrap().replace(&thread, "").into();
 
     match cmd {
         "bond" | "chess" | "compile" | "find" | "gato" | "wa" | "help" => { utilities::exec(cmd, msg, ctx, args).await? }
